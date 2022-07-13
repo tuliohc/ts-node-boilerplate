c5c3742e41f72c9d3d6a96541a8b66b08917fd77
Tulio <tuliohcosta@gmail.com>
2022-07-13 11:21:17 -0300

    feat: add server

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const router = express.Router()
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
+
